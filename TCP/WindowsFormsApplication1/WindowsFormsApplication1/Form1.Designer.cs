aa644ef00cc44eda74dd6c7fd8e663663ae5ef18
st33333 <526809654@qq.com>
2017-12-13 09:13:04 +0800

    TCP

---
+++ TCP/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ServerRecMsg = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPORT = new System.Windows.Forms.TextBox();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSendMsg = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnServerConn = new System.Windows.Forms.Button();
+            this.cboIpPort = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // ServerRecMsg
+            // 
+            this.ServerRecMsg.Location = new System.Drawing.Point(432, 265);
+            this.ServerRecMsg.Name = "ServerRecMsg";
+            this.ServerRecMsg.Size = new System.Drawing.Size(75, 23);
+            this.ServerRecMsg.TabIndex = 1;
+            this.ServerRecMsg.Text = "发送";
+            this.ServerRecMsg.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "IP:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(260, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "PORT:";
+            // 
+            // txtPORT
+            // 
+            this.txtPORT.Location = new System.Drawing.Point(301, 41);
+            this.txtPORT.Name = "txtPORT";
+            this.txtPORT.Size = new System.Drawing.Size(100, 21);
+            this.txtPORT.TabIndex = 4;
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(104, 41);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(100, 21);
+            this.txtIP.TabIndex = 5;
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(122, 117);
+            this.txtMsg.Multiline = true;
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(279, 120);
+            this.txtMsg.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(57, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "接受内容：";
+            // 
+            // txtSendMsg
+            // 
+            this.txtSendMsg.Location = new System.Drawing.Point(122, 265);
+            this.txtSendMsg.Name = "txtSendMsg";
+            this.txtSendMsg.Size = new System.Drawing.Size(279, 21);
+            this.txtSendMsg.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(57, 268);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "发送端：";
+            // 
+            // btnServerConn
+            // 
+            this.btnServerConn.Location = new System.Drawing.Point(432, 39);
+            this.btnServerConn.Name = "btnServerConn";
+            this.btnServerConn.Size = new System.Drawing.Size(75, 23);
+            this.btnServerConn.TabIndex = 0;
+            this.btnServerConn.Text = "启动服务";
+            this.btnServerConn.UseVisualStyleBackColor = true;
+            this.btnServerConn.Click += new System.EventHandler(this.btnServerConn_Click);
+            // 
+            // cboIpPort
+            // 
+            this.cboIpPort.FormattingEnabled = true;
+            this.cboIpPort.Location = new System.Drawing.Point(416, 79);
+            this.cboIpPort.Name = "cboIpPort";
+            this.cboIpPort.Size = new System.Drawing.Size(121, 20);
+            this.cboIpPort.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 357);
+            this.Controls.Add(this.cboIpPort);
+            this.Controls.Add(this.txtSendMsg);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.txtPORT);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ServerRecMsg);
+            this.Controls.Add(this.btnServerConn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button ServerRecMsg;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPORT;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSendMsg;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnServerConn;
+        private System.Windows.Forms.ComboBox cboIpPort;
+    }
+}
+
