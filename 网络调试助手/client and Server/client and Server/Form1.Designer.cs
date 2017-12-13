f0c29b6bf69530991c0829dbeb96f5f4cdf425f3
st33333 <526809654@qq.com>
2017-12-13 14:53:58 +0800

    网络调试助手

---
+++ 网络调试助手/client and Server/client and Server/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace client_and_Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tcpStatus = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbIP = new System.Windows.Forms.TextBox();
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnConnet = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtSendMsg = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtRecMsg = new System.Windows.Forms.TextBox();
+            this.cbIpPort = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tcpStatus
+            // 
+            this.tcpStatus.FormattingEnabled = true;
+            this.tcpStatus.Location = new System.Drawing.Point(457, 24);
+            this.tcpStatus.Name = "tcpStatus";
+            this.tcpStatus.Size = new System.Drawing.Size(121, 20);
+            this.tcpStatus.TabIndex = 0;
+            this.tcpStatus.SelectedIndexChanged += new System.EventHandler(this.tcpStatus_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(455, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "协议类型:";
+            // 
+            // tbIP
+            // 
+            this.tbIP.Location = new System.Drawing.Point(457, 69);
+            this.tbIP.Name = "tbIP";
+            this.tbIP.Size = new System.Drawing.Size(121, 21);
+            this.tbIP.TabIndex = 2;
+            // 
+            // tbPort
+            // 
+            this.tbPort.Location = new System.Drawing.Point(457, 119);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(121, 21);
+            this.tbPort.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(457, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "本地IP地址:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(457, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "本地端口号:";
+            // 
+            // btnConnet
+            // 
+            this.btnConnet.Location = new System.Drawing.Point(459, 165);
+            this.btnConnet.Name = "btnConnet";
+            this.btnConnet.Size = new System.Drawing.Size(119, 23);
+            this.btnConnet.TabIndex = 6;
+            this.btnConnet.Text = "开始监听";
+            this.btnConnet.UseVisualStyleBackColor = true;
+            this.btnConnet.Click += new System.EventHandler(this.btnConnet_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtRecMsg);
+            this.groupBox1.Location = new System.Drawing.Point(210, 24);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(224, 218);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "网络数据接收";
+            // 
+            // txtSendMsg
+            // 
+            this.txtSendMsg.Location = new System.Drawing.Point(210, 248);
+            this.txtSendMsg.Multiline = true;
+            this.txtSendMsg.Name = "txtSendMsg";
+            this.txtSendMsg.Size = new System.Drawing.Size(185, 53);
+            this.txtSendMsg.TabIndex = 8;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(413, 248);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(64, 40);
+            this.btnSend.TabIndex = 9;
+            this.btnSend.Text = "发送";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtRecMsg
+            // 
+            this.txtRecMsg.Location = new System.Drawing.Point(6, 17);
+            this.txtRecMsg.Multiline = true;
+            this.txtRecMsg.Name = "txtRecMsg";
+            this.txtRecMsg.Size = new System.Drawing.Size(199, 173);
+            this.txtRecMsg.TabIndex = 0;
+            // 
+            // cbIpPort
+            // 
+            this.cbIpPort.FormattingEnabled = true;
+            this.cbIpPort.Location = new System.Drawing.Point(321, 305);
+            this.cbIpPort.Name = "cbIpPort";
+            this.cbIpPort.Size = new System.Drawing.Size(74, 20);
+            this.cbIpPort.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 337);
+            this.Controls.Add(this.cbIpPort);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtSendMsg);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnConnet);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbPort);
+            this.Controls.Add(this.tbIP);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tcpStatus);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox tcpStatus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbIP;
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnConnet;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtSendMsg;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtRecMsg;
+        private System.Windows.Forms.ComboBox cbIpPort;
+    }
+}
+
---
+++ 网络调试助手/client and Server/client and Server/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace client_and_Server
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tcpStatus.Items.Add("TCP Serve");
+            tcpStatus.Items.Add("TCP Client");
+            tcpStatus.SelectedIndex = 0;
+            Control.CheckForIllegalCrossThreadCalls = false;
+
+
+
+
+        }
+
+        private void tcpStatus_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (tcpStatus.SelectedIndex == 1)
+            {
+                btnConnet.Text = "链接";
+            } else
+            {
+                btnConnet.Text = "开始监听";
+            }
+
+        }
+      
+        private void btnConnet_Click(object sender, EventArgs e)
+        {
+            IPAddress ip = IPAddress.Parse(tbIP.Text);
+            IPEndPoint point = new IPEndPoint(ip, int.Parse(tbPort.Text));
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            if (tcpStatus.SelectedIndex == 0)
+            {
+                try
+                {
+                    socket.Bind(point);
+                    //同一个时间点过来10个客户端，排队
+                    socket.Listen(10);
+                    ShowMsg("服务器开始监听");
+                    Thread thread = new Thread(AcceptInfo);
+                    thread.IsBackground = true;
+                    thread.Start(socket);
+                }
+                catch (Exception ex)
+                {
+                    ShowMsg(ex.Message);
+                }
+            }
+            else
+            {
+                try
+                {
+
+                    //连接到服务器
+
+                    client.Connect(point);
+
+                    ShowMsg("连接成功");
+
+                    ShowMsg("服务器" + client.RemoteEndPoint.ToString());
+
+    
+
+                    //连接成功后，就可以接收服务器发送的信息了
+
+                    Thread th = new Thread(ReceiveMsg);
+
+                    th.IsBackground = true;
+
+                    th.Start();
+
+                }
+
+            catch (Exception ex)
+
+                {
+
+                    ShowMsg(ex.Message);
+
+                }
+            }
+          
+        }
+        Dictionary<string, Socket> dic = new Dictionary<string, Socket>();
+        void AcceptInfo(object o)
+        {
+            Socket socket = o as Socket;
+            while (true)
+            {
+                //通信用socket
+                try
+                {
+                    //创建通信用的Socket
+                    Socket tSocket = socket.Accept();
+                    string point = tSocket.RemoteEndPoint.ToString();
+                    //IPEndPoint endPoint = (IPEndPoint)client.RemoteEndPoint;
+                    //string me = Dns.GetHostName();//得到本机名称
+                    //MessageBox.Show(me);
+                    ShowMsg(point + "连接成功！");
+                    cbIpPort.Items.Add(point);
+                    dic.Add(point, tSocket);
+                    //接收消息
+                    Thread th = new Thread(ReceiveMsg);
+                    th.IsBackground = true;
+                    th.Start(tSocket);
+                }
+                catch (Exception ex)
+                {
+                    ShowMsg(ex.Message);
+                    break;
+                }
+            }
+        }
+        void ReceiveMsg(object o)
+        {
+            if (tcpStatus.SelectedIndex == 0)
+            {
+                Socket client = o as Socket;
+                while (true)
+                {
+                    //接收客户端发送过来的数据
+                    try
+                    {
+                        //定义byte数组存放从客户端接收过来的数据
+                        byte[] buffer = new byte[1024 * 1024];
+                        //将接收过来的数据放到buffer中，并返回实际接受数据的长度
+                        int n = client.Receive(buffer);
+                        //将字节转换成字符串
+                        string words = Encoding.UTF8.GetString(buffer, 0, n);
+                        ShowMsg(client.RemoteEndPoint.ToString() + ":" + words);
+                    }
+                    catch (Exception ex)
+                    {
+                        ShowMsg(ex.Message);
+                        break;
+                    }
+                }
+            }else
+            {
+                while (true)
+
+                {
+
+                    try
+
+                    {
+
+                        byte[] buffer = new byte[1024 * 1024];
+
+                        int n = client.Receive(buffer);
+
+                        string s = Encoding.UTF8.GetString(buffer, 0, n);
+
+                        ShowMsg(client.RemoteEndPoint.ToString() + ":" + s);
+
+                    }
+
+                    catch (Exception ex)
+
+                    {
+
+                        ShowMsg(ex.Message);
+
+                        break;
+
+                    }
+
+                }
+
+            }
+        }
+        void ShowMsg(string msg)
+        {
+            txtRecMsg.AppendText(msg + "\r\n");
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //主窗体关闭时关闭子线程
+        }
+        Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (tcpStatus.SelectedIndex == 0)
+            {
+                try
+                {
+                    ShowMsg(txtRecMsg.Text);
+                    string ip = cbIpPort.Text;
+                    byte[] buffer = Encoding.UTF8.GetBytes(txtSendMsg.Text);
+                    dic[ip].Send(buffer);
+                    // client.Send(buffer);
+                }
+                catch (Exception ex)
+                {
+                    ShowMsg(ex.Message);
+                }
+            }
+            else
+            {
+                if (client != null)
+
+                {
+
+                    try
+
+                    {
+
+                        ShowMsg(txtRecMsg.Text);
+
+                        byte[] buffer = Encoding.UTF8.GetBytes(txtSendMsg.Text);
+
+                        client.Send(buffer);
+
+                    }
+
+                    catch (Exception ex)
+
+                    {
+
+                        ShowMsg(ex.Message);
+
+                    }
+                }
+            }
+        }
+
+    }
+}
