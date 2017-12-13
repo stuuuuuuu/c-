dee9dc2e6de457edad7f00605ec8824c2525ea1c
st33333 <526809654@qq.com>
2017-12-13 09:16:19 +0800

    串口

---
+++ UART3/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbSerial = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbBaudRate = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.串口设置 = new System.Windows.Forms.GroupBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.txtRcv = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.串口设置.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cbSerial
+            // 
+            this.cbSerial.FormattingEnabled = true;
+            this.cbSerial.Location = new System.Drawing.Point(71, 16);
+            this.cbSerial.Name = "cbSerial";
+            this.cbSerial.Size = new System.Drawing.Size(100, 20);
+            this.cbSerial.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "串口号:";
+            // 
+            // cbBaudRate
+            // 
+            this.cbBaudRate.FormattingEnabled = true;
+            this.cbBaudRate.Location = new System.Drawing.Point(238, 16);
+            this.cbBaudRate.Name = "cbBaudRate";
+            this.cbBaudRate.Size = new System.Drawing.Size(100, 20);
+            this.cbBaudRate.TabIndex = 17;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(190, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 12);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "波特率:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(382, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "打开串口";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // 串口设置
+            // 
+            this.串口设置.Controls.Add(this.checkBox2);
+            this.串口设置.Controls.Add(this.txtRcv);
+            this.串口设置.Controls.Add(this.checkBox1);
+            this.串口设置.Location = new System.Drawing.Point(25, 90);
+            this.串口设置.Name = "串口设置";
+            this.串口设置.Size = new System.Drawing.Size(511, 113);
+            this.串口设置.TabIndex = 20;
+            this.串口设置.TabStop = false;
+            this.串口设置.Text = "数据接收";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(105, 20);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(96, 16);
+            this.checkBox2.TabIndex = 26;
+            this.checkBox2.Text = "Auto NewLine";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // txtRcv
+            // 
+            this.txtRcv.Location = new System.Drawing.Point(11, 43);
+            this.txtRcv.Multiline = true;
+            this.txtRcv.Name = "txtRcv";
+            this.txtRcv.ReadOnly = true;
+            this.txtRcv.Size = new System.Drawing.Size(497, 55);
+            this.txtRcv.TabIndex = 0;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(21, 20);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(42, 16);
+            this.checkBox1.TabIndex = 19;
+            this.checkBox1.Text = "Hex";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkBox3);
+            this.groupBox2.Controls.Add(this.checkBox4);
+            this.groupBox2.Controls.Add(this.btnClear);
+            this.groupBox2.Controls.Add(this.btnSend);
+            this.groupBox2.Controls.Add(this.txtSend);
+            this.groupBox2.Location = new System.Drawing.Point(27, 209);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(511, 106);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "数据发送";
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(103, 29);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(96, 16);
+            this.checkBox3.TabIndex = 28;
+            this.checkBox3.Text = "Auto NewLine";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(19, 29);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(42, 16);
+            this.checkBox4.TabIndex = 27;
+            this.checkBox4.Text = "Hex";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(317, 61);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 13;
+            this.btnClear.Text = "清空数据";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(398, 61);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 12;
+            this.btnSend.Text = "发送数据";
+            this.btnSend.UseVisualStyleBackColor = true;
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(13, 61);
+            this.txtSend.Multiline = true;
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(298, 39);
+            this.txtSend.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(461, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Get:0";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(455, 318);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Send:0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(607, 348);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.串口设置);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cbBaudRate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbSerial);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.串口设置.ResumeLayout(false);
+            this.串口设置.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbSerial;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbBaudRate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox 串口设置;
+        private System.Windows.Forms.TextBox txtRcv;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSend;
+    }
+}
+
---
+++ UART3/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private SerialPort comn = new SerialPort();
+        private StringBuilder builder = new StringBuilder();//避免在事件处理方法中反复创建，定义到外面  
+        private long received_count = 0;//接收计数  
+        private long send_count = 0;//发送计数  
+        private bool Listening = false;//是否没有执行完invoke相关操作  
+        private bool PortClosing = false;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            String[] ports = SerialPort.GetPortNames();
+            Array.Sort(ports);
+            cbSerial.Items.AddRange(ports);
+            cbSerial.SelectedIndex = cbSerial.Items.Count > 0 ? 0 : -1;
+            cbBaudRate.SelectedIndex = cbBaudRate.Items.IndexOf("9600");
+            //初始化serialPort对象  
+            comn.NewLine = "/r/n";
+            comn.RtsEnable = true;//根据实际情况决定  
+            //添加事件注册  
+            comn.DataReceived += comn_DataReceived;
+
+        }
+        void comn_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (PortClosing)
+                return;//如果正常关闭，忽略操作，直接返回  
+            try
+            {
+                Listening = true;//设置标记，说明我已经开始处理数据，一会儿要使用系统的UI  
+                int n = comn.BytesToRead;//先记录下来，避免某种原因，认为的原因，操作几次之间时间长短，缓存不一致  
+                byte[] buf = new byte[n];//声明一个临时数组存储当前的串口数据  
+                received_count += n;//增加接收计数  
+                comn.Read(buf, 0, n);//读取缓冲数据  
+                builder.Clear();//清除字符串构造器的内容  
+                //因为要访问UI资源，所以需要使用invoke方式同步UI  
+                this.Invoke((EventHandler)(delegate
+                {
+                    //判断是否显示为16禁止  
+                    if (checkBox1.Checked)
+                    {
+                        //依次拼接出16进制字符串  
+                        foreach (byte b in buf)
+                        {
+                            builder.Append(b.ToString("X2") + "");
+                        }
+                    }
+                    else
+                    {
+                        //直接按ASCII规则转换成字符串  
+                        builder.Append(Encoding.ASCII.GetString(buf));
+                    }
+                    //追加的形式添加到文本框尾端，并滚动到最后  
+                    this.txtRcv.AppendText(builder.ToString());
+                    //修改接收计数  
+                    label2.Text = "Get:" + received_count.ToString();
+                }
+                ));
+            }
+            finally
+            {
+                Listening = false;//我用完了，UI可以关闭串口了  
+            }
+        }
+        private void checkBoxNewlineGet_CheckedChanged(object sender, EventArgs e)
+        {
+            txtRcv.WordWrap = checkBox2.Checked;
+        }
+        //  
+        private void ButtonSend_Click(object sender, EventArgs e)
+        {
+            //定义一个变量，记录发送了几个字节  
+            int n = 0;
+            //16进制发送  
+            if (checkBox4.Checked)
+            {
+                //我们不管规则了。如果写错了一些，我们是允许的，只用正则得到的有效地十六进制数  
+                MatchCollection mc = Regex.Matches(txtSend.Text, @"(?i[/da-f])");
+                List<byte> buf = new List<byte>();//填充到这个临时列表中  
+                //依次添加到列表中  
+                foreach (Match m in mc)
+                {
+                    buf.Add(byte.Parse(m.Value));
+                }
+                //转换列表为数组后发送  
+                comn.Write(buf.ToArray(), 0, buf.Count);
+                //记录发送的字节数  
+                n = buf.Count;
+            }
+            else
+            {
+                //包含换行符  
+                if (checkBox3.Checked)
+                {
+                    comn.WriteLine(txtSend.Text);
+                    n = txtSend.Text.Length + 2;
+                }
+                else
+                {
+                    comn.Write(txtSend.Text);
+                    n = txtSend.Text.Length;
+                }
+            }
+            send_count += n;//累加发送字节数  
+            label4.Text = "Send:" + send_count.ToString();//更新界面  
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
