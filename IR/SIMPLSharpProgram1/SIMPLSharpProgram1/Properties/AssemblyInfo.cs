2b2f87526584742fdcf62f8c9d273b5cf771dca7
st33333 <526809654@qq.com>
2018-01-13 10:55:53 +0800

    Remove

--- IR/SIMPLSharpProgram1/SIMPLSharpProgram1/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("SIMPLSharpProgram1")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SIMPLSharpProgram1")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyVersion("1.0.0.*")]
-
